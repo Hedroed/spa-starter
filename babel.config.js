2a918d4326559ee592c114cd6250c08d8ba28936
Hedroed <nathan.rydin@gmail.com>
2022-02-04 22:53:14 +0100

    wip

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,20 @@
+function hash(str) {
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+        var char = str.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return btoa(hash.toString(16));
+}
+
 module.exports = {
     "plugins": [
         ["react-css-modules", {
             generateScopedName(name, filename, css) {
-                return "__" + name;
-            }
+                console.log(name);
+                return hash(name.replace('\\', '')).slice(0, 6)
+            },
         }]
     ]
 }
